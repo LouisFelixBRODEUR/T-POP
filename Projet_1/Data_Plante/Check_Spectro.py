06bc4ea03f29d51854b11003c3dd146dd9551725
LouisFelixBRODEUR <lfbro1@ulaval.ca>
2025-02-19 16:11:33 -0500

    What?

--- Projet_1/Data_Plante/Check_Spectro.py
+++ Projet_1/Data_Plante/Check_Spectro.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -23,6 +24,9 @@ def get_tige_feuille_folders(base_dir):
                 tige_folders.append(full_path)
     return feuille_folders, tige_folders
 
+# c:\Users\louis\Documents\ULaval_S4\TPOP\GitWorkSpace\Projet_1\Data_Plante\Plante_Dragon
+# c:\\Users\\louis\\Documents\\ULaval_S4\\TPOP\\GitWorkSpace\\Projet_1\\Data_Plante\\Plante_Dragon
+
 # donne une liste de couple de list pour le data dans le folder [(x1,y1),(x2,y2),(x3,y3),...]
 def get_data_from_folder(base_dir):
     spectral_data = {}
@@ -44,34 +48,44 @@ def get_data_from_folder(base_dir):
 
 
 
-path_feuille = os.path.dirname(os.path.abspath(__file__))+"\\Plante_Dragon\\USB4F104151__0__12-54-15-542.txt"
-# path_tige = os.path.dirname(os.path.abspath(__file__))+"\\Plante_Limace\\Fente_0_08.csv"
-# path = os.path.dirname(os.path.abspath(__file__))+"\\Plante_Dragon\\USB4F104151__1__12-55-13-447.txt"
+# path_feuille_drag = os.path.dirname(os.path.abspath(__file__))+"\\Plante_Dragon\\Feuille_1\\USB4F104151__19__12-14-57-321.txt"
+# path_feuille_lim = os.path.dirname(os.path.abspath(__file__))+"\\Plante_Limace(Golden_Pothos)\\Feuille_1\\USB4F104151__0__11-38-47-480.txt"
+
+# # Read the file
+# def data_from(path):
+#     with open(path, "r") as file:
+#         lines = file.readlines()
+#     for i, line in enumerate(lines):
+#         if ">>>>>Begin Spectral Data<<<<<" in line:
+#             data_start = i + 1
+#             break
+#     return pd.read_csv(path, skiprows=data_start, delimiter="\t", names=["Wavelength", "Intensity"])
+
+# data_lim = data_from(path_feuille_lim)
+# data_drag = data_from(path_feuille_drag)
+# longueur_donde_lim = data_lim["Wavelength"]
+# longueur_donde_drag = data_drag["Wavelength"]
+# intensite_lim = data_lim["Intensity"]
+# intensite_drag = data_drag["Intensity"]
+
+# intensite_lim = intensite_lim/np.max(intensite_lim)
+# intensite_drag = intensite_drag/np.max(intensite_drag)
+
+Plant_DataBase = {}
+for Plante_Folder in get_Plante_folders(os.path.abspath(__file__)):
+    Plant_DataBase[Plante_Folder] = {}
 
 
-# path_background = os.path.dirname(os.path.abspath(__file__))+"\\BackGround\\Fente_0_08.csv"
 
-# file_path = "USB4F104151__19__12-14-57-321.txt"
 
-# Read the file
-with open(path_feuille, "r") as file:
-    lines = file.readlines()
 
-# Find the line where spectral data begins
-for i, line in enumerate(lines):
-    if ">>>>>Begin Spectral Data<<<<<" in line:
-        data_start = i + 1
-        break
-
-# Read spectral data into a DataFrame
-data = pd.read_csv(path_feuille, skiprows=data_start, delimiter="\t", names=["Wavelength", "Intensity"])
-
-# Plot the spectrum
-plt.figure(figsize=(10, 5))
-plt.plot(data["Wavelength"], data["Intensity"], label="Spectrum", color='b')
-plt.xlabel("Wavelength (nm)")
-plt.ylabel("Intensity")
-plt.title("Spectral Data Analysis")
-plt.legend()
-plt.grid()
-plt.show()
+# # Plot the spectrum
+# plt.figure(figsize=(10, 5))
+# plt.plot(longueur_donde_lim, intensite_lim, label="Limace", color='b')
+# plt.plot(longueur_donde_drag, intensite_drag, label="Dragon", color='r')
+# plt.xlabel("Wavelength (nm)")
+# plt.ylabel("Intensity")
+# plt.title("Spectral Data Analysis")
+# plt.legend()
+# plt.grid()
+# plt.show()
